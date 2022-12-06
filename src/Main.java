78decfeb26241cf7217fee84c98b6c50f208e471
nareshpaladugu <naresh.sears@gmail.com>
2022-12-06 17:50:26 -0600

    message1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        test1(20,30);
+        test2("tttttttttttttt");
+    }
+    public static void test1(int a,int b){
+        System.out.println(a+b);
+    }
+
+    public static void test2(String a){
+        System.out.println(a);
+    }
+
+}
